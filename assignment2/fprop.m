55b069a5b63971ef6e55d2163dd63eb1873ff657
Morteza Shahriari Nia <m.shahriarinia@gmail.com>
2014-12-30 15:27:30 -0500

    cleaning docs

--- assignment2/fprop.m
+++ assignment2/fprop.m
@@ -2,24 +2,29 @@ function [embedding_layer_state, hidden_layer_state, output_layer_state] = ...
   fprop(input_batch, word_embedding_weights, embed_to_hid_weights,...
   hid_to_output_weights, hid_bias, output_bias)
 % This method forward propagates through a neural network.
+% Note: first hidden layer (Word Embedding) is hidden1 and the second hidden
+% layer (Hidden Layer) is hidden2
+%
 % Inputs:
-%   input_batch: The input data as a matrix of size numwords X batchsize where,
-%     numwords is the number of words, batchsize is the number of data points.
-%     So, if input_batch(i, j) = k then the ith word in data point j is word
+%   input_batch: The input data as a numwords X batchsize matrix:
+%     numwords   : the number of words (In this case 3 as in 3 gram)
+%     batchsize  : the number of data points.
+%   So, if input_batch(i, j) = k then the ith word in data point j is word
 %     index k of the vocabulary.
 %
-%   word_embedding_weights: Word embedding as a matrix of size
-%     vocab_size X numhid1, where vocab_size is the size of the vocabulary
-%     numhid1 is the dimensionality of the embedding space.
+%   word_embedding_weights: Word embedding weights as a vocab_size X numhid1
+%   matrix:
+%     vocab_size : the size of the vocabulary (250 words in this example)
+%     numhid1    : dimensionality of the embedding space.
 %
 %   embed_to_hid_weights: Weights between the word embedding layer and hidden
-%     layer as a matrix of soze numhid1*numwords X numhid2, numhid2 is the
-%     number of hidden units.
+%   layer as a numhid1*numwords X numhid2 matrix: 
+%     numhid2    :   number of hidden units.
 %
 %   hid_to_output_weights: Weights between the hidden layer and output softmax
-%               unit as a matrix of size numhid2 X vocab_size
+%   unit as a numhid2 X vocab_size matrix:
 %
-%   hid_bias: Bias of the hidden layer as a matrix of size numhid2 X 1.
+%   hid_bias: Bias of the hidden layer as a numhid2 X 1 matrix.
 %
 %   output_bias: Bias of the output layer as a matrix of size vocab_size X 1.
 %
