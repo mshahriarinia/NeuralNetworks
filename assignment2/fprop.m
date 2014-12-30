41d40d0532b11f5b858e5205806f76ebfa78ef32
Morteza Shahriari Nia <m.shahriarinia@gmail.com>
2014-12-30 18:29:03 -0500

    commented train up to what embedding_layer_state means

--- assignment2/fprop.m
+++ assignment2/fprop.m
@@ -5,8 +5,11 @@ function [embedding_layer_state, hidden_layer_state, output_layer_state] = ...
 % Note: first hidden layer (Word Embedding) is hidden1 and the second hidden
 % layer (Hidden Layer) is hidden2
 %
+% We have a 4-gram from the first 3-grams we want to know what is the 4th
+% gram. 
+%
 % Inputs:
-%   input_batch: The input data as a numwords X batchsize matrix:
+%   input_batch: (mini batch of 3 grams) The input data as a numwords X batchsize matrix:
 %     numwords   : the number of words (In this case 3 as in 3 gram)
 %     batchsize  : the number of data points.
 %   So, if input_batch(i, j) = k then the ith word in data point j is word
@@ -15,11 +18,12 @@ function [embedding_layer_state, hidden_layer_state, output_layer_state] = ...
 %   word_embedding_weights: Word embedding weights as a vocab_size X numhid1
 %   matrix:
 %     vocab_size : the size of the vocabulary (250 words in this example)
-%     numhid1    : dimensionality of the embedding space.
+%     numhid1    : dimensionality of the embedding space. Dimensionality of embedding space; default = 50.
 %
-%   embed_to_hid_weights: Weights between the word embedding layer and hidden
+%   embed_to_hid_weights: Weights between the word embedding layer and
+%   hidden (from 3*50 neurons in embed layer to 200 neurons in the hidden layer)
 %   layer as a numhid1*numwords X numhid2 matrix: 
-%     numhid2    :   number of hidden units.
+%     numhid2    :   number of hidden units. Number of units in hidden layer; default = 200.
 %
 %   hid_to_output_weights: Weights between the hidden layer and output softmax
 %   unit as a numhid2 X vocab_size matrix:
@@ -39,15 +43,23 @@ function [embedding_layer_state, hidden_layer_state, output_layer_state] = ...
 %     vocab_size X batchsize
 %
 
-[numwords, batchsize] = size(input_batch);
-[vocab_size, numhid1] = size(word_embedding_weights);
+[numgrams, batchsize] = size(input_batch);
+[vocab_size, numhid1] = size(word_embedding_weights); % vocabulary embedding weights
 numhid2 = size(embed_to_hid_weights, 2);
 
 %% COMPUTE STATE OF WORD EMBEDDING LAYER.
 % Look up the inputs word indices in the word_embedding_weights matrix.
-embedding_layer_state = reshape(...
-  word_embedding_weights(reshape(input_batch, 1, []),:)',...
-  numhid1 * numwords, []);
+    
+    % stack the columns together one after another, one 3-gram at a time. like reshape([1 2 3 4; 7 8 9 0 ],1,[])
+    input_batch_reshaped = reshape(input_batch, 1, []); 
+   
+    % extract weights of each of the vocabulary instances in the input. one parameter set
+    % for each of the 300 words for each of the 50 neurons in Embedding layer, hence a 300*50
+    word_embedding_weights_lookedup = word_embedding_weights(input_batch_reshaped,:)';
+
+    % reshape 300*50 to 3*50*100 so all 3gram parameters in Embed layer are
+    % in line with  each other, and we have 100 minibatches
+    embedding_layer_state = reshape(word_embedding_weights_lookedup, numhid1 * numgrams, []);
 
 %% COMPUTE STATE OF HIDDEN LAYER.
 % Compute inputs to hidden units.
