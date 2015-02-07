cfc4c8acf100cd3a9bb85e1c3c473a1a868507d5
Morteza Shahriari Nia <m.shahriarinia@gmail.com>
2015-02-07 17:01:13 -0500

    Some commenting

--- assignment3/a3.m
+++ assignment3/a3.m
@@ -1,19 +1,29 @@
-% This version of the starter code was published on Tuesday November 8, 01:37 UTC. It is an improved version of the one that was published on November 6, 13:37 UTC.
+% This version of the starter code was published on Tuesday November 8, 01:37 UTC. 
+% It is an improved version of the one that was published on November 6, 13:37 UTC.
 
 function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, do_early_stopping, mini_batch_size)
+  
   warning('error', 'Octave:broadcast');
   if exist('page_output_immediately'), page_output_immediately(1); end
   more off;
+  
+  % Model is a struct of two matrices: input_to_hi[256*7]   hid_to_class[7*10]
   model = initial_model(n_hid);
+  
+  % load data
   from_data_file = load('data.mat');
   datas = from_data_file.data;
   n_training_cases = size(datas.training.inputs, 2);
+  
   if n_iters ~= 0,
     fprintf('Now testing the gradient on the whole training set... ');
+    
     test_gradient(model, datas.training, wd_coefficient);
   end
 
-  % optimization
+  % optimization:
+  % The optimizer that we're using is gradient descent with momentum, but
+  % we'll need to find good values for the learning rate and the momentum multiplier.
   theta = model_to_theta(model);
   momentum_speed = theta * 0;
   training_data_losses = [];
@@ -44,7 +54,9 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
     if mod(optimization_iteration_i, round(n_iters/10)) == 0,
       fprintf('After %d optimization iterations, training data loss is %f, and validation data loss is %f\n', optimization_iteration_i, training_data_losses(end), validation_data_losses(end));
     end
-    if optimization_iteration_i == n_iters, % check gradient again, this time with more typical parameters and with a different data size
+    
+    % check gradient again, this time with more typical parameters and with a different data size
+    if optimization_iteration_i == n_iters, 
       fprintf('Now testing the gradient on just a mini-batch instead of the whole training set... ');
       test_gradient(model, training_batch, wd_coefficient);
     end 
@@ -53,6 +65,7 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
     fprintf('Early stopping: validation loss was lowest after %d iterations. We chose the model that we had then.\n', best_so_far.after_n_iters);
     theta = best_so_far.theta;
   end
+  
   % the optimization is finished. Now do some reporting.
   model = theta_to_model(theta);
   if n_iters ~= 0,
@@ -78,6 +91,7 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
   end
 end
 
+%%
 function test_gradient(model, data, wd_coefficient)
   base_theta = model_to_theta(model);
   h = 1e-2;
@@ -96,9 +110,14 @@ function test_gradient(model, data, wd_coefficient)
   if any(isnan(analytic_gradient)) || any(isinf(analytic_gradient)),
      error('Your gradient computation produced a NaN or infinity. That is an error.')
   end
-  % We want to test the gradient not for every element of theta, because that's a lot of work. Instead, we test for only a few elements. If there's an error, this is probably enough to find that error.
-  % We want to first test the hid_to_class gradient, because that's most likely to be correct (it's the easier one).
-  % Let's build a list of theta indices to check. We'll check 20 elements of hid_to_class, and 80 elements of input_to_hid (it's bigger than hid_to_class).
+  
+  % We want to test the gradient not for every element of theta, because that's 
+  % a lot of work. Instead, we test for only a few elements. If there's an error, 
+  % this is probably enough to find that error.
+  % We want to first test the hid_to_class gradient, because that's most 
+  % likely to be correct (it's the easier one).
+  % Let's build a list of theta indices to check. We'll check 20 elements of 
+  % hid_to_class, and 80 elements of input_to_hid (it's bigger than hid_to_class).
   input_to_hid_theta_size = prod(size(model.input_to_hid));
   hid_to_class_theta_size = prod(size(model.hid_to_class));
   big_prime = 1299721; % 1299721 is prime and thus ensures a somewhat random-like selection of indices.
@@ -114,7 +133,9 @@ function test_gradient(model, data, wd_coefficient)
     contribution_weights = [1/280, -4/105, 1/5, -4/5, 4/5, -1/5, 4/105, -1/280];
     temp = 0;
     for contribution_index = 1:8,
-      temp = temp + loss(theta_to_model(base_theta + theta_step * contribution_distances(contribution_index)), data, wd_coefficient) * contribution_weights(contribution_index);
+      temp = temp + loss(theta_to_model(base_theta + ...
+          theta_step * contribution_distances(contribution_index)), data, wd_coefficient) * ...
+          contribution_weights(contribution_index);
     end
     fd_here = temp / h;
     diff = abs(analytic_here - fd_here);
@@ -129,10 +150,12 @@ function test_gradient(model, data, wd_coefficient)
   fprintf('Gradient test passed. That means that the gradient that your code computed is within 0.001%% of the gradient that the finite difference approximation computed, so the gradient calculation procedure is probably correct (not certainly, but probably).\n');
 end
 
+%% Logistic function
 function ret = logistic(input)
   ret = 1 ./ (1 + exp(-input));
 end
 
+%% log_sum_exp_over_rows
 function ret = log_sum_exp_over_rows(a)
   % This computes log(sum(exp(a), 1)) in a numerically stable way
   maxs_small = max(a, [], 1);
@@ -140,74 +163,134 @@ function ret = log_sum_exp_over_rows(a)
   ret = log(sum(exp(a - maxs_big), 1)) + maxs_small;
 end
 
+%% loss
+% model.input_to_hid is a matrix of size <number of hidden units> by <number of inputs i.e. 256>. 
+% It contains the weights from the input units to the hidden units.
+%
+% model.hid_to_class is a matrix of size <number of classes i.e. 10> by <number of hidden units>. 
+% It contains the weights from the hidden units to the softmax units.
+%
+% data.inputs is a matrix of size <number of inputs i.e. 256> by <number of data cases>. 
+% Each column describes a different data case. 
+%
+% data.targets is a matrix of size <number of classes i.e. 10> by <number of data cases>.
+% Each column describes a different data case. It contains a one-of-N encoding of the class,
+% i.e. one element in every column is 1 and the others are 0.
+
+% Before we can calculate the loss, we need to calculate a variety of intermediate values,
+% like the state of the hidden units. This is the forward pass, and you'll likely want to 
+% copy it into d_loss_by_d_model, because these values are also very useful for that function.
+
 function ret = loss(model, data, wd_coefficient)
-  % model.input_to_hid is a matrix of size <number of hidden units> by <number of inputs i.e. 256>. It contains the weights from the input units to the hidden units.
-  % model.hid_to_class is a matrix of size <number of classes i.e. 10> by <number of hidden units>. It contains the weights from the hidden units to the softmax units.
-  % data.inputs is a matrix of size <number of inputs i.e. 256> by <number of data cases>. Each column describes a different data case. 
-  % data.targets is a matrix of size <number of classes i.e. 10> by <number of data cases>. Each column describes a different data case. It contains a one-of-N encoding of the class, i.e. one element in every column is 1 and the others are 0.
-	 
-  % Before we can calculate the loss, we need to calculate a variety of intermediate values, like the state of the hidden units. This is the
-  % forward pass, and you'll likely want to copy it into d_loss_by_d_model, because these values are also very useful for that function.
-  hid_input = model.input_to_hid * data.inputs; % input to the hidden units, i.e. before the logistic. size: <number of hidden units> by <number of data cases>
-  hid_output = logistic(hid_input); % output of the hidden units, i.e. after the logistic. size: <number of hidden units> by <number of data cases>
-  class_input = model.hid_to_class * hid_output; % input to the components of the softmax. size: <number of classes, i.e. 10> by <number of data cases>
+  
+  % input to the hidden units, i.e. before the logistic.
+  % size: <number of hidden units> by <number of data cases>
+  hid_input = model.input_to_hid * data.inputs; 
+ 
+  % output of the hidden units, i.e. after the logistic. 
+  % size: <number of hidden units> by <number of data cases>
+  hid_output = logistic(hid_input); 
+  
+  % input to the components of the softmax. 
+  % size: <number of classes, i.e. 10> by <number of data cases>
+  class_input = model.hid_to_class * hid_output; 
   
   % The following three lines of code implement the softmax.
   % However, it's written differently from what the lectures say.
   % In the lectures, a softmax is described using an exponential divided by a sum of exponentials.
-  % What we do here is exactly equivalent (you can check the math or just check it in practice), but this is more numerically stable. 
+  % What we do here is exactly equivalent (you can check the math or just check it in practice), 
+  % but this is more numerically stable. 
   % "Numerically stable" means that this way, there will never be really big numbers involved.
-  % The exponential in the lectures can lead to really big numbers, which are fine in mathematical equations, but can lead to all sorts of problems in Octave.
-  % Octave isn't well prepared to deal with really large numbers, like the number 10 to the power 1000. Computations with such numbers get unstable, so we avoid them.
-  class_normalizer = log_sum_exp_over_rows(class_input); % log(sum(exp of class_input)) is what we subtract to get properly normalized log class probabilities. size: <1> by <number of data cases>
-  log_class_prob = class_input - repmat(class_normalizer, [size(class_input, 1), 1]); % log of probability of each class. size: <number of classes, i.e. 10> by <number of data cases>
-  class_prob = exp(log_class_prob); % probability of each class. Each column (i.e. each case) sums to 1. size: <number of classes, i.e. 10> by <number of data cases>
+  % The exponential in the lectures can lead to really big numbers, which are fine in mathematical 
+  % equations, but can lead to all sorts of problems in Octave.
+  % Octave isn't well prepared to deal with really large numbers, like the number 10 to the power 
+  % 1000. Computations with such numbers get unstable, so we avoid them.
   
-  classification_loss = -mean(sum(log_class_prob .* data.targets, 1)); % select the right log class probability using that sum; then take the mean over all data cases.
-  wd_loss = sum(model_to_theta(model).^2)/2*wd_coefficient; % weight decay loss. very straightforward: E = 1/2 * wd_coeffecient * theta^2
+  % log(sum(exp of class_input)) is what we subtract to get properly normalized log class probabilities. 
+  % size: <1> by <number of data cases>
+  class_normalizer = log_sum_exp_over_rows(class_input); 
+  
+  % log of probability of each class. size: <number of classes, i.e. 10> by <number of data cases>
+  log_class_prob = class_input - repmat(class_normalizer, [size(class_input, 1), 1]); 
+  
+  % probability of each class. Each column (i.e. each case) sums to 1. 
+  % size: <number of classes, i.e. 10> by <number of data cases>
+  class_prob = exp(log_class_prob); 
+  
+  % select the right log class probability using that sum; then take the mean over all data cases.
+  classification_loss = -mean(sum(log_class_prob .* data.targets, 1)); 
+  
+  % weight decay loss. very straightforward: E = 1/2 * wd_coeffecient * theta^2
+  wd_loss = sum(model_to_theta(model).^2)/2*wd_coefficient; 
   ret = classification_loss + wd_loss;
 end
 
-function ret = d_loss_by_d_model(model, data, wd_coefficient)
-  % model.input_to_hid is a matrix of size <number of hidden units> by <number of inputs i.e. 256>
-  % model.hid_to_class is a matrix of size <number of classes i.e. 10> by <number of hidden units>
-  % data.inputs is a matrix of size <number of inputs i.e. 256> by <number of data cases>
-  % data.targets is a matrix of size <number of classes i.e. 10> by <number of data cases>
+%% d_loss_by_d_model
+% model.input_to_hid is a matrix of size <number of hidden units> by <number of inputs i.e. 256>
+% model.hid_to_class is a matrix of size <number of classes i.e. 10> by <number of hidden units>
+% data.inputs is a matrix of size <number of inputs i.e. 256> by <number of data cases>
+% data.targets is a matrix of size <number of classes i.e. 10> by <number of data cases>
 
-  % The returned object is supposed to be exactly like parameter <model>, i.e. it has fields ret.input_to_hid and ret.hid_to_class. However, the contents of those matrices are gradients (d loss by d model parameter), instead of model parameters.
-	 
-  % This is the only function that you're expected to change. Right now, it just returns a lot of zeros, which is obviously not the correct output. Your job is to change that.
+% The returned object is supposed to be exactly like parameter <model>, i.e. it has fields 
+% ret.input_to_hid and ret.hid_to_class. However, the contents of those matrices are gradients 
+% (d loss by d model parameter), instead of model parameters.
+
+% This is the only function that you're expected to change. Right now, it just returns a 
+% lot of zeros, which is obviously not the correct output. Your job is to change that.
+function ret = d_loss_by_d_model(model, data, wd_coefficient)
   ret.input_to_hid = model.input_to_hid * 0;
   ret.hid_to_class = model.hid_to_class * 0;
 end
 
+%% model_to_theta
+% This function takes a model (or gradient in model form), and turns it into 
+% one long vector. See also theta_to_model.
 function ret = model_to_theta(model)
-  % This function takes a model (or gradient in model form), and turns it into one long vector. See also theta_to_model.
   input_to_hid_transpose = transpose(model.input_to_hid);
   hid_to_class_transpose = transpose(model.hid_to_class);
   ret = [input_to_hid_transpose(:); hid_to_class_transpose(:)];
 end
 
+%% theta_to_model
+% This function takes a model (or gradient) in the form of one long vector 
+% (maybe produced by model_to_theta), and restores it to the structure format, 
+% i.e. with fields .input_to_hid and .hid_to_class, both matrices.
 function ret = theta_to_model(theta)
-  % This function takes a model (or gradient) in the form of one long vector (maybe produced by model_to_theta), and restores it to the structure format, i.e. with fields .input_to_hid and .hid_to_class, both matrices.
   n_hid = size(theta, 1) / (256+10);
   ret.input_to_hid = transpose(reshape(theta(1: 256*n_hid), 256, n_hid));
   ret.hid_to_class = reshape(theta(256 * n_hid + 1 : size(theta,1)), n_hid, 10).';
 end
 
+%% initial_model
 function ret = initial_model(n_hid)
   n_params = (256+10) * n_hid;
   as_row_vector = cos(0:(n_params-1));
-  ret = theta_to_model(as_row_vector(:) * 0.1); % We don't use random initialization, for this assignment. This way, everybody will get the same results.
+  
+  % We don't use random initialization, for this assignment. 
+  % This way, everybody will get the same results.
+  ret = theta_to_model(as_row_vector(:) * 0.1); 
 end
 
+%% classification_performance
+% This returns the fraction of data cases that is incorrectly classified by the model.
 function ret = classification_performance(model, data)
-  % This returns the fraction of data cases that is incorrectly classified by the model.
-  hid_input = model.input_to_hid * data.inputs; % input to the hidden units, i.e. before the logistic. size: <number of hidden units> by <number of data cases>
-  hid_output = logistic(hid_input); % output of the hidden units, i.e. after the logistic. size: <number of hidden units> by <number of data cases>
-  class_input = model.hid_to_class * hid_output; % input to the components of the softmax. size: <number of classes, i.e. 10> by <number of data cases>
   
-  [dump, choices] = max(class_input); % choices is integer: the chosen class, plus 1.
-  [dump, targets] = max(data.targets); % targets is integer: the target class, plus 1.
+  % input to the hidden units, i.e. before the logistic. 
+  % size: <number of hidden units> by <number of data cases>
+  hid_input = model.input_to_hid * data.inputs; 
+  
+  % output of the hidden units, i.e. after the logistic. 
+  % size: <number of hidden units> by <number of data cases>
+  hid_output = logistic(hid_input); 
+  
+  % input to the components of the softmax. 
+  % size: <number of classes, i.e. 10> by <number of data cases>
+  class_input = model.hid_to_class * hid_output; 
+  
+   % choices is integer: the chosen class, plus 1.
+  [dump, choices] = max(class_input);
+  
+  % targets is integer: the target class, plus 1.
+  [dump, targets] = max(data.targets); 
   ret = mean(double(choices ~= targets));
 end
--- assignment3/main.m
+++ assignment3/main.m
@@ -1,6 +1,12 @@
 % I use this for my experiments and illustrations
+
+  
+a3(1e7, 7, 10, 0, 0, false, 4)
+
+  %% Visualize data
+  
 from_data_file = load('data.mat');
-  datas = from_data_file.data;
+data = from_data_file.data;
   
 for i=1:50
     idx = i*10+4; % idx = i; % for all the numbers
