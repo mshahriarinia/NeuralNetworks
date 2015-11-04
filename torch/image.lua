1e4db3dae620c651b26062b6e586534d578453f5
mshahriarinia <info@mshahriarinia.com>
2015-11-04 13:43:44 -0500

    Add comment

--- torch/image.lua
+++ torch/image.lua
@@ -2,14 +2,18 @@
 To run:
     $ qlua image.lua
 
-for using qtlua, start torch in your terminal with:
+For using qtlua, start torch in your terminal with:
     $ qlua
 instead of
     $ th
 
- you can also run like this:
+Or, you can also run like this:
     $ qlua -e "require('trepl')()"    
     th>  require('image.lua')
+
+Or, from within qlua
+    $ qlua
+    > dofile 'getstarted.lua'
 
 --]]
 
@@ -22,3 +26,10 @@ image.display(i)
 
 image.display{image={image.lena(), image.lena()},gui=false}
 
+--  visualizing internal states, and convolution filters:
+n = nn.SpatialConvolution(1,64,16,16)
+image.display(n.weight)
+
+n = nn.SpatialConvolution(1,16,12,12)
+res = n:forward(image.rgb2y(image.lena()))
+image.display(res:view(16,1,501,501))
