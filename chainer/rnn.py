5ff9286f0fdab1855ac2fd37e1201b3f362e2e69
Morteza Shahriari Nia <m.shahriarinia@gmail.com>
2015-11-20 13:42:22 -0500

    update rnn

--- chainer/rnn.py
+++ chainer/rnn.py
@@ -30,8 +30,43 @@ Network:
     w_{hidden_to_output}: 1000x50
         w_{hidden_to_output} * 50 + bias   -> 1000x1
 
-
-
 """
+
+model = FunctionSet(
+    embed  = F.EmbedID(1000, 100),
+    x_to_h = F.Linear(100,   50),
+    h_to_h = F.Linear( 50,   50),
+    h_to_y = F.Linear( 50, 1000),
+)
+optimizer = optimizers.SGD()
+optimizer.setup(model)
+
+# The forward computation is simply as a for loop over input sequence which is a list of integer array.:
+
+volatile=False
+h = Variable(np.zeros((1, 50), dtype=np.float32), volatile=volatile)
+loss   = 0
+count  = 0
+seqlen = len(x_list[1:])
+
+for cur_word, next_word in zip(x_list, x_list[1:]):
+    word = Variable(cur_word, volatile=volatile) # put current word into a Chainer variable
+    t    = Variable(next_word, volatile=volatile) # target word (next word in sequence)
+    
+    x    = F.tanh(model.embed(word))
+    h    = F.tanh(model.x_to_h(x) + model.h_to_h(h))
+    y    = model.h_to_y(h)
+    loss = F.softmax_cross_entropy(y, t)
+
+    loss += new_loss
+    count += 1
+    if count % 30 == 0 or count == seqlen:   # At each 30 steps, backprop takes place at the accumulated loss.
+        optimizer.zero_grads() # Do not forget to call Optimizer.zero_grads() before the backward computation!
+        loss.backward()
+        loss.unchain_backward() # deletes the computation history backward from the accumulated loss.
+        optimizer.update()
+
+# Note that the first dimension of h and x_list is always the mini-batch size. The mini-batch size is assumed to be 1 here. 
+
 
 
