777630aa8098dd8b46f045fb9cf717cec4b90c7f
Morteza Shahriari Nia <m.shahriarinia@gmail.com>
2015-11-20 15:49:38 -0500

    clarify chainer's funciton details

--- chainer/rnn.py
+++ chainer/rnn.py
@@ -30,6 +30,66 @@ Network:
     w_{hidden_to_output}: 1000x50
         w_{hidden_to_output} * 50 + bias   -> 1000x1
 
+===============
+More on chainer functions:
+    class Linear(in_size, out_size, wscale=1, bias=0, nobias=False, initialW=None, initial_bias=None)
+        The weight matrix W has shape (out_size, in_size). 
+        - Linear.forward(x) returns Y=XW^T+b
+        - Lienar.backward(x,gy)  where gy is gradient of y, it calculates gradientes, etc
+     	
+	Parameters:	
+	    in_size ("int"): Dimension of input "vector".
+	    out_size (int): Dimension of output vector.
+	    wscale (float): Scaling factor of the weight matrix.
+	    bias (float): Initial bias value.
+	    nobias (bool): If True, then this function does not use the bias.
+	    initialW (2-D array): Initial weight value. If None, then this function uses to initialize wscale.
+	    initial_bias (1-D array): Initial bias value. If None, then this function uses to initialize bias.
+
+        It provides a funciton linear(x, W, b) to be used without class.
+    
+
+    
+    class lstm(c_prev, x)
+	Long Short-Term Memory units as an activation function.	This function implements LSTM units with forget gates. Let the previous cell state "cprev" and the incoming signal "x".
+
+	First, the incoming signal x is split into four arrays a,i,f,o of the same shapes along the second axis. It means that x's second axis must have 4 times the length of cprev, where:
+		a : sources of cell input (Ct_prime)
+		i : sources of input gate
+		f : sources of forget gate
+		o : sources of output gate
+
+	Second, it computes outputs as:
+		c=tanh(a)sigmoid(i)+cprev*sigmoid(f),           # f=Wf*xt+Ufh_{t-1}+bc , so it assuems this is given. OR, others as well: 
+		h=tanh(c)sigmoid(o).
+
+	These are returned as a tuple of two variables.
+	Parameters:	
+
+	    c_prev (Variable) – Variable that holds the previous cell state. The cell state should be a zero array or the output of the previous call of LSTM.
+	    x (Variable) – Variable that holds the incoming signal. It must have the second dimension four times of that of the cell state,
+
+	Returns:	
+
+	Two Variable objects c and h. c is the updated cell state. h indicates the outgoing signal.
+	Return type:	
+
+	tuple
+
+	See the original paper proposing LSTM with forget gates: Long Short-Term Memory in Recurrent Neural Networks.
+
+	Example
+
+	Assuming y is the current input signal, c is the previous cell state, and h is the previous output signal from an lstm function. Each of y, c and h has n_units channels. Most typical preparation of x is:
+
+	>>> model = FunctionSet(w=F.Linear(n_units, 4 * n_units),
+	...                     v=F.Linear(n_units, 4 * n_units),
+	...                     ...)
+	>>> x = model.w(y) + model.v(h)
+	>>> c, h = F.lstm(c, x)
+
+	It corresponds to calculate the input sources a,i,f,o from the current input y and the previous output h. Different parameters are used for different kind of input sources.
+     
 """
 
 model = FunctionSet(
